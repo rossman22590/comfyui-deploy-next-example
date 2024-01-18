21f8d5e651eef4c624264f7fbe5ce87245998405
Ross Cohen <rosscohen1990@gmail.com>
2024-01-18 00:49:34 -0500

    Update comfy-deploy.ts

--- src/lib/comfy-deploy.ts
+++ src/lib/comfy-deploy.ts
@@ -21,7 +21,7 @@ const uploadFileTypes = z.object({
 })
 
 export class ComfyDeployClient {
-  apiBase: string = "https://comfyui-deploy-11b9.vercel.app/api";
+  apiBase: string = "https://api.myapps.ai/api";
   apiToken: string;
 
   constructor({ apiBase, apiToken }: { apiBase?: string; apiToken: string }) {
